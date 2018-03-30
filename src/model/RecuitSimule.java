723b6a74e77ef5d130cb8589c61a95f72fabdc71
Valentin Bonnal <co.merce@hotmail.fr>
2018-03-30 21:47:13 +0200

    critMetropolis

--- src/model/RecuitSimule.java
+++ src/model/RecuitSimule.java
@@ -1,9 +1,21 @@
 package model;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 /**
  * Created by Valentin.
  */
 public class RecuitSimule {
 	private int temperature;
 	private int temperatureFinale;
+
+	public RecuitSimule(int temperature, int temperatureFinale) {
+		this.temperature = temperature;
+		this.temperatureFinale = temperatureFinale;
+	}
+
+	public boolean critMetropolis(int delta, float temperature) {
+		if (delta < 0) return true;
+		return ThreadLocalRandom.current().nextFloat() < Math.exp(-delta / temperature);
+	}
 }
